aaf637909c18a00b64b1dd2502369e435625953b
18382268894 <qq1005914310@live.com>
2018-11-23 03:51:00 +0800

    获取文件内容相关

--- model/file/file.go
+++ model/file/file.go
@@ -18,6 +18,8 @@ import (
 	"dataServer/conf"
 	"mime/multipart"
 	"io/ioutil"
+	"net/http"
+	"io"
 )
 
 
@@ -26,42 +28,6 @@ type FileInfo struct {
 	*RealFileInfo
 	Content string
 	Size   int64
-}
-
-type RealFileInfo struct {
-	FileName string `json:"file_name"` //文件名称  文件名称为一个全局唯一id
-	FilePath string `json:"file_path"` //文件路径 文件路径为：日期
-	Hosts []string `json:"host"`   //保存的主机位置，可能保存在多个主机上
-}
-
-type UserFileInfo struct {
-	Username   string    `json:"username"`    //用户名
-	FileName   string    `json:"file_name"`   //文件名
-	Path       string    `json:"path"`        //文件路径
-	CreateTime time.Time `json:"create_time"` //生成时间
-	Sha string       	`json:"sha"` 		  //文件内容的散列值
-}
-
-
-//将realFileInfo序列化
-func(f *RealFileInfo)Marshal()(string,error){
-	return marshal(f)
-}
-
-//将realFileInfo序列化
-func(f *UserFileInfo)Marshal()(string,error){
-	return marshal(f)
-}
-
-
-func(f *RealFileInfo)GetKey(sha string)(string){
-	key := fmt.Sprintf("%s/%s",conf.ETCD_FILEPATH_PREFIX,sha)
-	return key
-}
-
-func(f *UserFileInfo)GetKey()(string){
-	key := fmt.Sprintf("%s/%s/%s/%s",conf.ETCD_FILEPATH_PREFIX,f.Username,f.Path,f.FileName)
-	return key
 }
 
 
@@ -95,22 +61,21 @@ func GetFileFromFeader(username,path string,header *multipart.FileHeader)( *File
 }
 
 
-//添加文件(很完美)
+//添加文件
 func (fileInfo *FileInfo)AddFile()error{
 	var relfilinfoval ,userfileInfoval string
 	var err error
 	var resp *clientv3.TxnResponse
 
-	ukey := fileInfo.UserFileInfo.GetKey()
-	rkey := fileInfo.RealFileInfo.GetKey(fileInfo.Sha)
+	ukey := fileInfo.UserFileInfo.CreateKey()
+	rkey := fileInfo.RealFileInfo.CreateKey(fileInfo.Sha)
 
-	if userfileInfoval,err = fileInfo.UserFileInfo.Marshal();err != nil{
+	if userfileInfoval,err = marshal(fileInfo.UserFileInfo);err != nil{
 		return err
 	}
-	if relfilinfoval,err = fileInfo.RealFileInfo.Marshal();err != nil{
+	if relfilinfoval,err = marshal(fileInfo.RealFileInfo);err != nil{
 		return err
 	}
-
 	txn := myetcd.KV.Txn(context.TODO())
 	txn.If(clientv3.Compare(clientv3.CreateRevision(ukey),"=",0),
 		clientv3.Compare(clientv3.CreateRevision(rkey),"=",0)).
@@ -138,8 +103,8 @@ func (fileInfo *FileInfo)AddFile()error{
 
 //创建文件
 func (fileInfo *FileInfo)writeFile()(error){
-	//建立文件目录
-	dirName := fmt.Sprintf("/Users/mok/localDB%s",time.Now().Format("2006-01-02"))  //可以写进配置文件里
+	//建立文件目录 todo:应该把dirName用环境变量传进来
+	dirName := fmt.Sprintf("%s/%s",conf.DATA_DIRNAME,time.Now().Format("2006-01-02"))
 	err := os.MkdirAll(dirName,0777)
 	if err != nil{
 		return err
@@ -162,11 +127,92 @@ func (fileInfo *FileInfo)writeFile()(error){
 	fileInfo.RealFileInfo = &RealFileInfo{
 		FileName:uuid,
 		FilePath:dirName,
-		Hosts:[]string{conf.SERVER_ADDR},
+		Host:conf.SERVER_ADDR,
 	}
 	return err
 }
 
+//获取文件的信息和内容(传入参数为etcd中保存userinfo的key，/prefix/user/path/filename)
+func GetFileInfo(username,path,filename string)(*FileInfo,error){
+	var fileinfo = &FileInfo{}
+	if username == "" || path == "" || filename == ""{
+		return nil,fmt.Errorf("传入的参数无效")
+	}
+	//去etcd中获取到userinfo数据
+	key := fmt.Sprintf("%s/%s/%s/%s",conf.ETCD_FILEPATH_PREFIX,username,path,filename)
+	var err error
+	if err = fileinfo.UserFileInfo.GetUserInfo(key);err != nil{
+		return nil,err
+	}
+	//然后通过sha值去获取到文件存放的信息
+	if err = fileinfo.RealFileInfo.GetRealFileInfo(fileinfo.Sha);err != nil{
+		return nil,err
+	}
+
+	fileUrl := fmt.Sprintf("%s/%s",fileinfo.RealFileInfo.FilePath,fileinfo.RealFileInfo.FileName)
+	//判断主机和当前主机是否相同
+	if fileinfo.Host == conf.SERVER_ADDR{
+			//相同就直接去打开该主机路径下的文件
+		if fileinfo.Content,err = readContent(fileUrl);err!=nil{
+			return nil,err
+		}
+	}else {
+		//文件不在该主机上，就要去发送http请求到另一个客户端，获取文件内容
+		client := http.Client{Timeout:5*time.Second}  //todo:写进配置文件里
+		url := fmt.Sprintf("%s?fileUrl=%s",fileinfo.Host,fileUrl)
+		request,err := http.NewRequest(http.MethodGet,url,nil)
+		if err != nil{
+			return nil,err
+		}
+		resp,err := client.Do(request)
+		if err != nil{
+			return nil,err
+		}
+		content,err := readContentFromBody(resp.Body)
+		if err != nil{
+			return nil,err
+		}
+		fileinfo.Content = content
+	}
+	return fileinfo,nil
+}
+
+func readContent(fileUrl string)(string,error){
+	f,err := os.Open(fileUrl)
+	defer f.Close()
+	if err != nil{
+		return "",err
+	}
+	//读取文件内容
+ 	data,err :=ioutil.ReadAll(f)
+	if err != nil{
+		return "",err
+	}
+	return string(data),nil
+}
+
+func readContentFromBody(r io.ReadCloser)(string,error){
+	defer r.Close()
+	data,err := ioutil.ReadAll(r)
+	if err != nil{
+		return "",err
+	}
+	return string(data),nil
+}
+
+//处理别的主机文件请求
+func GetFileinByOther(fileUrl string)(string,error){
+	f,err := os.Open(fileUrl)
+	defer f.Close()
+	if err != nil{
+		return "",err
+	}
+	data,err := ioutil.ReadAll(f)
+	if err != nil{
+		return "",err
+	}
+	return string(data),nil
+}
 
 func marshal(v interface{})(string,error){
 	data,err := json.Marshal(v)
